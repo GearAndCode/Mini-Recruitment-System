b4bc4b8f4dd663742f28c16c5ac75342a8a17907
GearAndCode <hareematif2007@gmail.com>
2026-07-15 09:17:22 +0300

    Fix API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,34 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://mini-recruitment-system-production.up.railway.app/api",
 });
-
-API.interceptors.request.use((config) => {
-  const token =
-    localStorage.getItem("rec_token") ||
-    sessionStorage.getItem("rec_token");
-
-  console.log("TOKEN:", token);
-  console.log("REQUEST:", config.method, config.url);
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
-API.interceptors.response.use(
-  (response) => {
-    console.log("SUCCESS:", response.config.url, response.status, response.data);
-    return response;
-  },
-  (error) => {
-    console.log("FAILED:", error.response?.status);
-    console.log(error.response?.data);
-    return Promise.reject(error);
-  }
-);
 
 export default API;
